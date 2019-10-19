b1c4709c6bab4df83c762c2bcdfe44e58605b040
andanao <47489123+andanao@users.noreply.github.com>
2019-10-19 18:46:03 -0400

    works with 2 engines

--- fools_win.py
+++ fools_win.py
@@ -4,18 +4,19 @@ import chess.engine
 
 async def main():
     transport, engine = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
-    transport, engine_crap = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
-
+    # transport, engine_crap = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
+    
+    tlim = .005
     board = chess.Board()
     while not board.is_game_over():
-        result = await engine.play(board, chess.engine.Limit(time=0.0100))
+        result = await engine.play(board, chess.engine.Limit(time=tlim))
         board.push(result.move)
-        print(board.uci(result.move), file=open("stockfish_test.pgn", "a+"))
+        print(board.uci(result.move), file=open("stockfish_test1.pgn", "a+"))
         if board.is_game_over():
             break
-        result = await engine_crap.play(board, chess.engine.Limit(time=0.0100))
+        result = await engine.play(board, chess.engine.Limit(time=tlim))
         board.push(result.move)
-        print(board.uci(result.move), file=open("stockfish_test.pgn", "a+"))
+        print(board.uci(result.move), file=open("stockfish_test1.pgn", "a+"))
     # while board.is_game_over():
     #     print(board.move_stack, file=open("stockficsh_test.pgn", "w+"), end="\n\n")
 
