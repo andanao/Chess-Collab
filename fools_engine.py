a6d010dbe36b41dc773964c4bdbcaef64b429005
andanao <47489123+andanao@users.noreply.github.com>
2019-10-19 19:36:55 -0400

    kinda got a working engine?

--- fools_engine.py
+++ fools_engine.py
@@ -9,10 +9,11 @@ class fools:
         self.moveset = ["d2d4", "e1c3", "c1f4", "c3c7"]
         self.turn = 0
     
-    def self.play(board, tlim):
+    def play(self, board, tlim):
         if self.turn < 4:
-            optimal_play = chess.Move.from_uci(self.moveset[turn])
+            optimal_play = chess.Move.from_uci(self.moveset[self.turn])
         else:
             optimal_play = chess.Move.null()
         self.turn += 1
+        print(self.turn)
         return optimal_play
--- fools_win.py
+++ fools_win.py
@@ -1,26 +1,36 @@
-import asyncio
+# import asyncio
 import chess
 import chess.engine
+import fools_engine
 
-async def main():
-    transport, engine = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
-    # transport, engine_crap = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
-    
-    tlim = .005
-    board = chess.Board()
-    while not board.is_game_over():
-        result = await engine.play(board, chess.engine.Limit(time=tlim))
-        board.push(result.move)
-        print(board.uci(result.move), file=open("stockfish_test1.pgn", "a+"))
-        if board.is_game_over():
-            break
-        result = await engine.play(board, chess.engine.Limit(time=tlim))
-        board.push(result.move)
-        print(board.uci(result.move), file=open("stockfish_test1.pgn", "a+"))
-    # while board.is_game_over():
-    #     print(board.move_stack, file=open("stockficsh_test.pgn", "w+"), end="\n\n")
+# def main():
+print("\n\n\n\n\n\n\n")
+engine = chess.engine.SimpleEngine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
+engine2 = fools_engine.fools()
+# transport2, engine_crap = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x32")
+# transport, engine_crap = await chess.engine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
+print("", file=open("stockfish_test5.pgn", "w+"), end="")
 
-    await engine.quit()
 
-asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
-asyncio.run(main())
+tlim = .005
+board = chess.Board()
+while not board.is_game_over():
+    result2 = engine2.play(board, chess.engine.Limit(time=tlim))
+    board.push(result2)
+    print(board.uci(result2), file=open("stockfish_test5.pgn", "a+"))
+    if board.is_game_over():
+        break
+    result = engine.play(board, chess.engine.Limit(time=tlim))
+    board.push(result.move)
+    print(board.uci(result.move), file=open("stockfish_test5.pgn", "a+"))
+
+    # result = await engine_crap.play(board, chess.engine.Limit(time=tlim))
+    # board.push(result.move)
+    # print(board.uci(result.move), file=open("stockfish_test1.pgn", "a+"))
+# while board.is_game_over():
+#     print(board.move_stack, file=open("stockficsh_test.pgn", "w+"), end="\n\n")
+
+engine.quit()
+# engine2.quit()
+# asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+# asyncio.run(main())
